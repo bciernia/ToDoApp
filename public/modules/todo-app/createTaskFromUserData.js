4a3c0e574041b77e7237cefa06e5fe84f3e2c251
Bartek <cierniabartek0802@gmail.com>
2023-01-26 21:25:19 +0100

    deleted task description prop

--- public/modules/todo-app/createTaskFromUserData.js
+++ public/modules/todo-app/createTaskFromUserData.js
@@ -2,13 +2,11 @@ const taskForm = document.querySelector(".new-task-from");
 
 export const createTaskFromUserData = () => {
     const taskName = taskForm.elements["task-name"].value;
-    const taskDesc = taskForm.elements["task-desc"].value
     const taskImportance = taskForm.elements["task-importance"].value;
 
     return {
         "taskId": 0,
         "taskName": taskName,
-        "taskDesc": taskDesc,
         "taskImportance": taskImportance,
         "isTaskFinished": false,
     }
