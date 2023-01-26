4fc9d4bf5b6cb41ccc74aca7646aba2cf6bf311d
Bartek <cierniabartek0802@gmail.com>
2023-01-26 21:25:04 +0100

    added edit button onClickHandler

--- public/modules/todo-app/showUserTasks.js
+++ public/modules/todo-app/showUserTasks.js
@@ -1,14 +1,18 @@
 import {createListRow, createDiv, createBtn, createParagraph} from "../design-system/core.js";
 import {deleteTask} from "./deleteTask.js";
-import {renderTasks} from "./getTasks.js";
 
-const taskList = document.querySelector('.task-list');
+const taskList = document.querySelector('.current-task-list');
 
 export const showUserTasks = (tasks) => {
     taskList.innerText = "";
 
     tasks.sort((taskA, taskB) => (taskA.taskImportance > taskB.taskImportance) ? 1 : -1)
         .forEach(task => {
+            const editChosenTask = () => {
+                console.log("Edytujemy" + task.taskName);
+                //TODO patch current element
+            }
+
             const removeTaskFromList = () => {
                 console.log(`${task.name} is deleted, ID: ${task.taskId}`);
                 deleteTask(task.taskId);
@@ -17,12 +21,13 @@ export const showUserTasks = (tasks) => {
             const li = createListRow([]);
             const div = createDiv(["task", `task-importance-background-color-${task.taskImportance}`]);
             const p = createParagraph(task.taskName, []);
-            const btnEdit = createBtn('E', ["btn", "btn-edit-task"]);
+            const btnEdit = createBtn('E', ["btn", "btn-edit-task"],
+                editChosenTask);
             const btnRemove = createBtn('X', ["btn", "btn-remove-task"],
                 removeTaskFromList);
 
-            div.appendChild(btnEdit);
             div.appendChild(p);
+            div.appendChild(btnEdit);
             div.appendChild(btnRemove);
 
             li.appendChild(div);
