8632116960f712905ff7b410bbe61b3efc8681c0
Bartek <cierniabartek0802@gmail.com>
2023-01-29 16:29:06 +0100

    added deadline to task

--- public/modules/todo-app/createTaskFromUserData.js
+++ public/modules/todo-app/createTaskFromUserData.js
@@ -3,10 +3,12 @@ const taskForm = document.querySelector(".new-task-from");
 export const createTaskFromUserData = () => {
     const taskName = taskForm.elements["task-name"].value;
     const taskImportance = taskForm.elements["task-importance"].value;
+    const taskDeadline = taskForm.elements["task-deadline"].value;
 
     return {
         "taskId": 0,
         "taskName": taskName,
+        "taskDeadline": taskDeadline,
         "taskImportance": taskImportance,
         "isTaskFinished": false,
     }
