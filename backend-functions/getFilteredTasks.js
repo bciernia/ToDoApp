f15f6c250041328bb96c8cadd7f4c1fba8effcb1
Bartek <cierniabartek0802@gmail.com>
2023-02-05 00:05:11 +0100

    fixed filtering

--- backend-functions/getFilteredTasks.js
+++ backend-functions/getFilteredTasks.js
@@ -1,7 +1,15 @@
 const getFilteredTasks = (tasks, filter) => {
-    if(filter === "all-tasks") return tasks;
 
-    return tasks.filter(task => task.taskImportance === filter);
+    switch (filter){
+        case "all-tasks":
+            return tasks;
+
+        case "4-finished":
+            return tasks.filter(task => task.isTaskFinished === true);
+
+        default:
+            return tasks.filter(task => task.taskImportance === filter && task.isTaskFinished === false);
+    }
 }
 
 module.exports = {
