e414fa7959ff9ff14993ed5c697bb2786a41299b
Bartek <cierniabartek0802@gmail.com>
2023-02-05 01:08:24 +0100

    added alerts in modal

--- public/modules/todo-app/createTaskFromUserData.js
+++ public/modules/todo-app/createTaskFromUserData.js
@@ -1,3 +1,5 @@
+import {createParagraph} from "../core/domManager.js";
+
 const taskForm = document.querySelector(".new-task-from");
 
 export const createTaskFromUserData = () => {
@@ -5,6 +7,28 @@ export const createTaskFromUserData = () => {
     const taskImportance = taskForm.elements["task-importance"].value;
     const isTaskDeadlineAvailable = taskForm.elements["task-deadline-confirm"].checked;
     const date = new Date(document.querySelector('.new-task-datepicker').value);
+    const alertParagraph = taskForm.getElementsByClassName('error-message');
+
+    //ALERT MESSAGES
+    const emptyTaskName = createParagraph("*Task name cannot be empty", ["error-message"]);
+    const wrongDate = createParagraph("*Task date cannot be earlier than today", ["error-message"]);
+    const emptyDate = createParagraph("*Task date cannot be empty", ["error-message"]);
+
+    const alerts = [];
+
+    Array.from(alertParagraph).forEach(alert => taskForm.removeChild(alert));
+
+    alerts.length = 0;
+
+    if (taskName === "") alerts.push(emptyTaskName);
+
+    if(isTaskDeadlineAvailable && new Date(date).toString() === "Invalid Date") alerts.push(emptyDate);
+
+    if (Date.now() >= date) alerts.push(wrongDate);
+
+    alerts.forEach(alert => taskForm.appendChild(alert));
+
+    if (alerts.length > 0) return null;
 
     return {
         "taskName": taskName,
